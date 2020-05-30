e83a4039dd480ef3a6fe8afc87539be433bd216d
goyoac <goyoac@gmail.com>
2020-05-30 03:41:49 +0200

    Added rhds dict

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Dataset(object):
     """
     roar = "I'm a dataset"
 
-    def __init__(self, name='', url='', path=''):
+    def __init__(self, name="", url="", path="", hash_code=""):
         """
         Initializes the dataset with supplied values
 
@@ -37,6 +37,7 @@ class Dataset(object):
         self.name = name
         self.url = url
         self.path = path
+        self.hash_code = hash_code
 
 def hash_for_directory(path, hashfunc=hashlib.sha1):
     """
@@ -81,73 +82,130 @@ def main():
                 "sarmis-s2",
                 "sarmis-s3")
 
-    data = ("Raw data",
-            "Laser scans",
-            "RGB-D data",
-            "Reconstructed scenes",
-            "Labeled scenes",
-            "Labeled RGB-D data",
-            "Characterized elements",
-            "2D geometric maps",
-            "2D geometric maps + logs",
-            "Home's topologies")
-
-    urls = ("https://ananas.isa.uma.es:10002/sharing/PAJxeUT0q",
-            "https://ananas.isa.uma.es:10002/sharing/pMVKQb5hl",
-            "https://ananas.isa.uma.es:10002/sharing/sJUC06jFJ",
-            "https://ananas.isa.uma.es:10002/sharing/sFCGRu1LN",
-            "https://ananas.isa.uma.es:10002/sharing/KS6kscXb3",
-            "https://ananas.isa.uma.es:10002/sharing/jVVI92AJn",
-            "https://ananas.isa.uma.es:10002/sharing/t6zVblP3w",
-            "https://ananas.isa.uma.es:10002/sharing/PUZHG28p6",
-            "https://ananas.isa.uma.es:10002/sharing/VLWRJUJGY",
-            "https://ananas.isa.uma.es:10002/sharing/EBXypqYAV")
-
-    paths = ("Robot@Home-dataset_raw_data-plain_text-all",
-             "Robot@Home-dataset_laser_scans-plain_text-all",
-             "Robot@Home-dataset_rgbd_data-plain_text-all",
-             "Robot@Home-dataset_reconstructed-scenes_plain-text_all",
-             "Robot@Home-dataset_labelled-scenes_plain-text_all",
-             "Robot@Home-dataset_labelled-rgbd-data_plain-text_all",
-             "Robot@Home-dataset_characterized-elements",
-             "Robot@Home-dataset_2d_geometric_maps",
-             "Robot@Home-dataset_2d_geometric_maps+logs",
-             "Robot@Home-dataset_homes-topologies")
 
     rhds = []
     rhds.append(Dataset("Raw data",
                         "https://ananas.isa.uma.es:10002/sharing/PAJxeUT0q",
-                        "Robot@Home-dataset_raw_data-plain_text-all"))
+                        "Robot@Home-dataset_raw_data-plain_text-all",
+                        "4823b61180bbf8ce5458ad43ad709069edb0e8f3"))
+
     rhds.append(Dataset("Laser scans",
                         "https://ananas.isa.uma.es:10002/sharing/pMVKQb5hl",
-                        "Robot@Home-dataset_laser_scans-plain_text-all"))
+                        "Robot@Home-dataset_laser_scans-plain_text-all",
+                        "0f188931b2bce1926d0faaac13be78614749ec72"))
+
     rhds.append(Dataset("RGB-D data",
                         "https://ananas.isa.uma.es:10002/sharing/sJUC06jFJ",
-                        "Robot@Home-dataset_rgbd_data-plain_text-all"))
+                        "Robot@Home-dataset_rgbd_data-plain_text-all",
+                        "b934e53d16580a62e0f4f1532d1efaa0567232ae"))
+
     rhds.append(Dataset("Reconstructed scenes",
                         "https://ananas.isa.uma.es:10002/sharing/sFCGRu1LN",
-                        "Robot@Home-dataset_reconstructed-scenes_plain-text_all"))
+                        "Robot@Home-dataset_reconstructed-scenes_plain-text_all",
+                        "5dc5aed9dfebf6e14890eb2418d3f518d0f55c6d"))
+
     rhds.append(Dataset("Labeled scenes",
                         "https://ananas.isa.uma.es:10002/sharing/KS6kscXb3",
-                        "Robot@Home-dataset_labelled-scenes_plain-text_all"))
+                        "Robot@Home-dataset_labelled-scenes_plain-text_all",
+                        "394dc6c8f4d19b2887007e6ee66f2e7cb64f930f"))
+
     rhds.append(Dataset("Labeled RGB-D data",
                         "https://ananas.isa.uma.es:10002/sharing/jVVI92AJn",
-                        "Robot@Home-dataset_labelled-rgbd-data_plain-text_all"))
+                        "Robot@Home-dataset_labelled-rgbd-data_plain-text_all",
+                        "6834f930a16bb5d968d55b0b647703d952384e91"))
+
     rhds.append(Dataset("Characterized elements",
                         "https://ananas.isa.uma.es:10002/sharing/t6zVblP3w",
-                        "Robot@Home-dataset_characterized-elements"))
+                        "Robot@Home-dataset_characterized-elements",
+                        "9d46bc3b33d2c6b84c04bd3db12cc415c17b4ae8"))
+
     rhds.append(Dataset("2D geometric maps",
                         "https://ananas.isa.uma.es:10002/sharing/PUZHG28p6",
-                        "Robot@Home-dataset_2d_geometric_maps"))
+                        "Robot@Home-dataset_2d_geometric_maps",
+                        "a00bb33bc628e0fdb9df6822915b9eafcf67998f"))
+
     rhds.append(Dataset("2D geometric maps + logs",
                         "https://ananas.isa.uma.es:10002/sharing/VLWRJUJGY",
-                        "Robot@Home-dataset_2d_geometric_maps+logs"))
+                        "Robot@Home-dataset_2d_geometric_maps+logs",
+                        "f8b3dadd9181291a59f589033521708d4399d12b"))
+
     rhds.append(Dataset("Home's topologies",
                         "https://ananas.isa.uma.es:10002/sharing/EBXypqYAV",
-                        "Robot@Home-dataset_homes-topologies"))
+                        "Robot@Home-dataset_homes-topologies",
+                        "652087d30c05ff4eaec9a0770307a2ced7fe5064"))
 
-    h = fire.Fire(hash_for_directory)
-    
+    # h = fire.Fire(hash_for_directory)
+
+    rhds = {}
+
+    rhds["raw"] = Dataset(
+      "Raw data",
+      "https://ananas.isa.uma.es:10002/sharing/PAJxeUT0q",
+      "Robot@Home-dataset_raw_data-plain_text-all",
+      "4823b61180bbf8ce5458ad43ad709069edb0e8f3")
+
+    rhds["lsrscan"] = Dataset(
+      "Laser scans",
+      "https://ananas.isa.uma.es:10002/sharing/pMVKQb5hl",
+      "Robot@Home-dataset_laser_scans-plain_text-all",
+      "0f188931b2bce1926d0faaac13be78614749ec72")
+
+    rhds["rgbd"] = Dataset(
+      "RGB-D data",
+      "https://ananas.isa.uma.es:10002/sharing/sJUC06jFJ",
+      "Robot@Home-dataset_rgbd_data-plain_text-all",
+      "b934e53d16580a62e0f4f1532d1efaa0567232ae")
+
+    rhds["recscn"] = Dataset(
+      "Reconstructed scenes",
+      "https://ananas.isa.uma.es:10002/sharing/sFCGRu1LN",
+      "Robot@Home-dataset_reconstructed-scenes_plain-text_all",
+      "5dc5aed9dfebf6e14890eb2418d3f518d0f55c6d")
+
+    rhds["lblscn"] = Dataset(
+      "Labeled scenes",
+      "https://ananas.isa.uma.es:10002/sharing/KS6kscXb3",
+      "Robot@Home-dataset_labelled-scenes_plain-text_all",
+      "394dc6c8f4d19b2887007e6ee66f2e7cb64f930f")
+
+    rhds["lblrgbd"] = Dataset(
+      "Labeled RGB-D data",
+      "https://ananas.isa.uma.es:10002/sharing/jVVI92AJn",
+      "Robot@Home-dataset_labelled-rgbd-data_plain-text_all",
+      "6834f930a16bb5d968d55b0b647703d952384e91")
+
+    rhds["chelmnt"] = Dataset(
+      "Characterized elements",
+      "https://ananas.isa.uma.es:10002/sharing/t6zVblP3w",
+      "Robot@Home-dataset_characterized-elements",
+      "9d46bc3b33d2c6b84c04bd3db12cc415c17b4ae8")
+
+    rhds["2dgeomap"] = Dataset(
+      "2D geometric maps",
+      "https://ananas.isa.uma.es:10002/sharing/PUZHG28p6",
+      "Robot@Home-dataset_2d_geometric_maps",
+      "a00bb33bc628e0fdb9df6822915b9eafcf67998f")
+
+    rhds["2dgeomapl"] = Dataset(
+      "2D geometric maps + logs",
+      "https://ananas.isa.uma.es:10002/sharing/VLWRJUJGY",
+      "Robot@Home-dataset_2d_geometric_maps+logs",
+      "f8b3dadd9181291a59f589033521708d4399d12b")
+
+    rhds["hometopo"] = Dataset(
+      "Home's topologies",
+      "https://ananas.isa.uma.es:10002/sharing/EBXypqYAV",
+      "Robot@Home-dataset_homes-topologies",
+      "652087d30c05ff4eaec9a0770307a2ced7fe5064")
+
+
+    print (len(rhds))
+    for x in rhds:
+      print(x)
+      print(rhds[x].name)
+
+# https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
+# https://pypi.org/project/humanize/
 
     return 0
 
