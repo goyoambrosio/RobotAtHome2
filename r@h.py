5975c036899be60f4bc8f51baa56b7cefca75af3
goyoac <goyoac@gmail.com>
2020-06-16 03:04:49 +0200

    Added DatasetUnitLaserScans (unexpected)

--- r@h.py
+++ r@h.py
@@ -1840,7 +1840,7 @@ class Dataset():
 
         def __load_data(self):
             home_sessions = self.HomeSessions()
-            home_folders = os.listdir(self.path)
+            home_folders = sorted(os.listdir(self.path))
             # print(home_folders)
             for home_folder in home_folders:
                 words = home_folder.strip().split('-')
@@ -1849,8 +1849,9 @@ class Dataset():
                                  words[len_of_words - 1]
                 # print(home_folder)
                 rooms = self.Rooms()
-                room_files = os.listdir(self.path + '/' + home_folder + '/' +
-                                        home_subfolder)
+                room_files = sorted(os.listdir(self.path + '/' +
+                                               home_folder + '/' +
+                                               home_subfolder))
                 # print(room_files)
                 for room_file in room_files:
                     if room_file.endswith('.txt'):
@@ -1897,6 +1898,361 @@ class Dataset():
                                          room_folder_path, sensors)
                         rooms.append(room)
                         # print(room)
+                # print(rooms)
+                # input("Press Enter to continue...")
+                home_session = self.HomeSession(home_subfolder, rooms)
+                home_sessions.append(home_session)
+
+            return home_sessions
+
+        def __str__(self):
+            s = ""
+            return super().__str__() + s
+
+    class DatasetUnitLaserScans(DatasetUnit):
+        class HomeSession():
+            def __init__(self, name, rooms):
+                self.name = name
+                self.rooms = rooms
+
+            def __str__(self):
+                s = '\t' + self.name
+                return s
+
+            def __repr__(self):
+                s = "<HomeSession instance (" + self.name + ")>"
+                return s
+
+        class HomeSessions(list):
+
+            def __init__(self):
+                pass
+
+            def __str__(self):
+                s = ''
+                for item in self:
+                    s += str(item) + "\n"
+                return s
+
+            def __repr__(self):
+                s = "<HomesSessions list instance (" + str(len(self)) + \
+                    " items)>"
+                return s
+
+            def get_names(self):
+                return [home.name for home in self]
+
+        class Room():
+            def __init__(self, name, folder_path, sensor_sessions):
+                self.name = name
+                self.folder_path = folder_path
+                self.sensor_sessions = sensor_sessions
+
+            def __str__(self):
+                s = '\t' + self.name + '  folder: /' + self.folder_path + \
+                    ' # sensor_sessions: ' + str(len(self.sensor_sessions))
+                return s
+
+            def __repr__(self):
+                s = "<Room instance (" + self.name + ")>"
+                return s
+
+        class Rooms(list):
+
+            def __init__(self):
+                pass
+
+            def __str__(self):
+                s = ''
+                for item in self:
+                    s += str(item) + "\n"
+                return s
+
+            def __repr__(self):
+                s = "<Rooms list instance (" + str(len(self)) + \
+                    " items)>"
+                return s
+
+        class SensorSession():
+            def __init__(self, name, folder_path, sensor_observations):
+                self.name = name
+                self.folder_path = folder_path
+                self.sensor_observations = sensor_observations
+
+            def __str__(self):
+                s = '\t' + self.name + '  folder: /' + self.folder_path + \
+                    ' # observations: ' + str(len(self.sensor_observations))
+                return s
+
+            def __repr__(self):
+                s = "<SensorSession instance (" + self.name + ")>"
+                return s
+
+        class SensorSessions(list):
+
+            def __init__(self):
+                pass
+
+            def __str__(self):
+                s = ''
+                for item in self:
+                    s += str(item) + "\n"
+                return s
+
+            def __repr__(self):
+                s = "<SensorSessions list instance (" + str(len(self)) + \
+                    " items)>"
+                return s
+
+        class Sensor():
+            def __init__(self, id='0', name='undefined',
+                         sensor_pose_x='0', sensor_pose_y='0', sensor_pose_z='0',
+                         sensor_pose_yaw='0', sensor_pose_pitch='0', sensor_pose_roll='0',
+                         time_stamp='0', files=[], path=""):
+                self.id = id
+                self.name = name
+                self.sensor_pose_x = sensor_pose_x
+                self.sensor_pose_y = sensor_pose_y
+                self.sensor_pose_z = sensor_pose_x
+                self.sensor_pose_yaw = sensor_pose_yaw
+                self.sensor_pose_pitch = sensor_pose_pitch
+                self.sensor_pose_roll = sensor_pose_roll
+                self.time_stamp = time_stamp
+                self.files = files
+                self.path = path
+
+            def __str__(self):
+                s = '\t' + self.id + ', ' + self.name + ', ' + \
+                    self.sensor_pose_x + ', ' + \
+                    self.sensor_pose_y + ', ' + \
+                    self.sensor_pose_z + ', ' + \
+                    self.sensor_pose_yaw + ', ' + \
+                    self.sensor_pose_pitch + ', ' + \
+                    self.sensor_pose_roll + ', ' + \
+                    self.time_stamp + ', ' + \
+                    str(self.files)
+                return s
+
+            def __repr__(self):
+                s = "<Sensor instance (" + self.name + ")>"
+                return s
+
+            def load_files(self):
+
+                """
+                The path should be the room.path of the room to which it
+                belongs
+                """
+                sensor_observation_files = os.listdir(self.path)
+                indexes = [i for i, j in enumerate(sensor_observation_files) if j.split('_')[0] == self.id]
+                """
+                If files is empty the file names are loaded
+                If not, file names are already loaded (cached)
+                """
+                if len(self.files) == 0:
+                    for i in indexes:
+                        self.files.append(sensor_observation_files[i])
+                    """
+                    According to some files features self instance is casted
+                    to a specialized one.
+                    """
+                    if 'scan' in sensor_observation_files[i]:
+                        self.__class__ = Dataset.DatasetUnitLaserScans.SensorLaserScanner
+
+                return self.files
+
+            def get_type(self):
+                """
+                Example:
+                with a type like this:
+                <class '__main__.Dataset.DatasetUnitRawData.SensorCamera'>
+                return this:
+                SensorCamera
+                """
+                return str(type(self)).split('.')[-1][:-2]
+
+        class Sensors(list):
+
+            def __init__(self):
+                pass
+
+            def __str__(self):
+                s = ''
+                for item in self:
+                    s += str(item) + "\n"
+                return s
+
+            def __repr__(self):
+                s = "<Sensors list instance (" + str(len(self)) + \
+                    " items)>"
+                return s
+
+        class SensorLaserScanner(Sensor):
+            def __init__(self, id='0', name='undefined',
+                         sensor_pose_x='0', sensor_pose_y='0', sensor_pose_z='0',
+                         sensor_pose_yaw='0', sensor_pose_pitch='0', sensor_pose_roll='0',
+                         time_stamp='0', files=[]):
+                """ Calls the super class __init__"""
+                super().__init__(id, name,
+                                 sensor_pose_x, sensor_pose_y, sensor_pose_z,
+                                 sensor_pose_yaw, sensor_pose_pitch, sensor_pose_roll,
+                                 time_stamp, files)
+
+            def __str__(self):
+                s = '\t' + 'Laser scanner: '
+                return s + super().__str__()
+
+            def __repr__(self):
+                s = "<SensorLaserScanner instance (" + self.name + ")>"
+                return s
+
+            def get_laser_scan(self):
+                laser_scan = Dataset.DatasetUnitLaserScans.LaserScan()
+                with open(self.path + '/' + self.files[0], "r") as file_handler:
+                    line_number = 0
+                    for line in file_handler:
+                        words = line.strip().split()
+                        if words[0] != '#':
+                            line_number += 1
+                            if line_number == 1:
+                                laser_scan.aperture = words[0]
+                            elif line_number == 2:
+                                laser_scan.max_range = words[0]
+                            elif line_number == 4:
+                                laser_scan.vector_of_scans = words
+                            elif line_number == 5:
+                                laser_scan.vector_of_valid_scans = words
+                return laser_scan
+
+        class SensorLaserScanners(list):
+
+            def __init__(self):
+                pass
+
+            def __str__(self):
+                s = ''
+                for item in self:
+                    s += str(item) + "\n"
+                return s
+
+            def __repr__(self):
+                s = "<SensorLaserScanners list instance (" + str(len(self)) + \
+                    " items)>"
+                return s
+
+        class LaserScan():
+            def __init__(self, aperture='0', max_range='0',
+                         vector_of_scans=[], vector_of_valid_scans=[]):
+                self.aperture = aperture
+                self.max_range = max_range
+                self.vector_of_scans = vector_of_scans
+                self.vector_of_valid_scans = vector_of_valid_scans
+
+            def __str__(self):
+                s = '\t' + 'Laser scan: ' +  self.aperture + ', ' + \
+                    self.max_range + ', num. of scans: ' + \
+                    str(len(self.vector_of_scans)) + ', valid: ' + \
+                    str(len(self.vector_of_valid_scans))
+                return s
+
+            def __repr__(self):
+                s = "<LaserScan instance (" + self.name + ")>"
+                return s
+
+        class LaserScans(list):
+
+            def __init__(self):
+                pass
+
+            def __str__(self):
+                s = ''
+                for item in self:
+                    s += str(item) + "\n"
+                return s
+
+            def __repr__(self):
+                s = "<LaserScans list instance (" + str(len(self)) + \
+                    " items)>"
+                return s
+
+        def __init__(self, name="", url="", path="", expected_hash_code="",
+                     expected_size=0):
+            """ Calls the super class __init__"""
+            super().__init__(name, url, path, expected_hash_code,
+                             expected_size)
+
+            self.home_sessions = self.__load_data()
+
+        def __load_data(self):
+            home_folders = sorted(os.listdir(self.path))
+            home_sessions = self.HomeSessions()
+            # print(home_folders)
+            for home_folder in home_folders:
+                words = home_folder.strip().split('-')
+                len_of_words = len(words)
+                home_subfolder = words[len_of_words-2] + '-' + \
+                                 words[len_of_words - 1]
+                # print(home_folder)
+                room_folders = sorted(os.listdir(self.path + '/' +
+                                                 home_folder + '/' +
+                                                 home_subfolder))
+                # print(room_folders)
+                rooms = self.Rooms()
+                for room_folder in room_folders:
+                    # print(room_folder)
+                    room_folder_path = self.path + '/' + home_folder + \
+                                       '/' + home_subfolder + '/' + \
+                                       room_folder
+                    sensor_session_files = sorted(os.listdir(room_folder_path))
+                    # print(sensor_session_files)
+                    sensor_sessions = self.SensorSessions()
+                    for sensor_session_file in sensor_session_files:
+                        if sensor_session_file.endswith('.txt'):
+                            # print(sensor_session_file)
+                            sensor_session_folder_path = room_folder_path + '/' + \
+                                               sensor_session_file.split('.')[0]
+                            sensor_session_file_path = room_folder_path + '/' + \
+                                               sensor_session_file
+                            # print(sensor_session_folder_path)
+                            # print(sensor_session_file_path)
+                            sensors = self.Sensors()
+                            with open(sensor_session_file_path, "r") as file_handler:
+                                for line in file_handler:
+                                    words = line.strip().split()
+                                    if words[0] != '#':
+                                        """
+                                        Read a line with the following structure:
+                                        words  0 : [Observation_id]
+                                        words  1 : [sensor_label]
+                                        words  2 : [sensor_pose_x]
+                                        words  3 : [sensor_pose_y]
+                                        words  4 : [sensor_pose_z]
+                                        words  5 : [sensor_pose_yaw]
+                                        words  6 : [sensor_pose_pitch]
+                                        words  7 : [sensor_pose_roll]
+                                        words  8 : [time-stamp]
+                                        """
+                                        # print(words[0])
+                                        sensor = self.Sensor(words[0],
+                                                             words[1],
+                                                             words[2],
+                                                             words[3],
+                                                             words[4],
+                                                             words[5],
+                                                             words[6],
+                                                             words[7],
+                                                             words[8],
+                                                             [],
+                                                             sensor_session_folder_path)
+
+                                        sensors.append(sensor)
+                            sensor_session = self.SensorSession(sensor_session_file.split('.')[0],
+                                                                sensor_session_folder_path, sensors)
+                            sensor_sessions.append(sensor_session)
+                    room = self.Room(room_folder,
+                                     room_folder_path, sensor_sessions)
+                    rooms.append(room)
+                    # print(room)
                 # print(rooms)
                 # input("Press Enter to continue...")
                 home_session = self.HomeSession(home_subfolder, rooms)
@@ -1988,7 +2344,7 @@ class Dataset():
           "4823b61180bbf8ce5458ad43ad709069edb0e8f3",
           20002442369)
 
-        self.unit["lsrscan"] = self.DatasetUnitRawData(
+        self.unit["lsrscan"] = self.DatasetUnitLaserScans(
           "Laser scans",
           "https://ananas.isa.uma.es:10002/sharing/pMVKQb5hl",
           "Robot@Home-dataset_laser_scans-plain_text-all",
@@ -2000,6 +2356,7 @@ class Dataset():
         self.home_2dgeomaps = self.unit["2dgeomap"].homes
         self.home_topologies = self.unit["hometopo"].homes
         self.home_sessions_sensor_observations = self.unit["raw"].home_sessions
+        self.home_sessions_laser_scans = self.unit["lsrscan"].home_sessions
 
     def __str__(self):
 
@@ -2047,19 +2404,20 @@ def main():
     print(home_sessions[0].name)
     print(home_sessions[0].rooms[0].name)
     print(home_sessions[0].rooms[0].folder_path)
+    print(home_sessions[0].rooms[0].sensor_sessions)
 
     path = home_sessions[0].rooms[0].folder_path
 
-    sensor = home_sessions[0].rooms[0].sensor_observations[0]
+    sensor = home_sessions[0].rooms[0].sensor_sessions[0].sensor_observations[0]
     print(sensor)
     print(type(sensor))
     print(sensor.load_files())
     print(sensor.get_type())
     print(type(sensor))
-    laser_scan = sensor.get_laser_scan(path)
+    laser_scan = sensor.get_laser_scan()
     print(laser_scan)
-    #print(laser_scan.vector_of_scans)
-    #print(laser_scan.vector_of_valid_scans)
+    # print(laser_scan.vector_of_scans)
+    # print(laser_scan.vector_of_valid_scans)
 
     """ About raw data """
     """
