ae16906e5a9e78bad30c805ff9067b24772dbc9c
goyoac <goyoac@gmail.com>
2020-10-05 17:10:41 +0200

    New LICENSE file

--- setup.py
+++ setup.py
@@ -5,19 +5,25 @@
 Setup for RobotAtHome API
 """
 
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name='robotathome',
-    version='0.1',
-    author='Gregorio Ambrosio',
-    author_email='gambrosio@uma.es',
-    packages=find_packages(exclude=['tests*']),
-    license='LICENSE.md',
-    description='This package provides Python API that assists in loading, \
-    parsing, and visualizing the annotations in Robot@Home Dataset',
-    long_description=open('README.md').read(),
-    install_requires=['os', 'hashlib', 'humanize', 'wget',
-                      'ssl', 'cv2', 'numpy', 'sys'],
-    url='https://github.com/goyoambrosio/RobotAtHome_API'
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="robotathome",
+    version="0.1.1",
+    author="G. Ambrosio-Cestero",
+    author_email="gambrosio@uma.es",
+    description="'This package provides Python API that assists in loading, parsing, and visualizing the annotations in Robot@Home Dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/goyoambrosio/RobotAtHome_API",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
